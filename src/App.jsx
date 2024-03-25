ed9f50df5c38748062f3fc002a2bf58c51f20ac7
leewebstechnologies <aliuajibade317@gmail.com>
2024-03-25 18:11:56 +0100

    file renamed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div>
-      <h1>Hello World!</h1>
+      <h1>Welcome to Emirates Flight Booking Website</h1>
       <h2>Airline Booking App</h2>
       <button>Submit</button>
     </div>
